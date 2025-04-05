495cccac2aaf4d5267a00305b4a0516e990f7827
Kushal8616 <Kanjanap@flemingcollege.ca>
2025-04-05 17:27:08 -0400

    Initial portfolio setup

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Resume Modal Functionality
 document.getElementById("resumeButton").onclick = function() {
-    document.getElementById("resumeModal").style.display = "block";
-  }
-  
-  document.getElementsByClassName("close")[0].onclick = function() {
+  document.getElementById("resumeModal").style.display = "block";
+}
+
+document.getElementsByClassName("close")[0].onclick = function() {
+  document.getElementById("resumeModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById("resumeModal")) {
     document.getElementById("resumeModal").style.display = "none";
   }
-  
-  window.onclick = function(event) {
-    if (event.target == document.getElementById("resumeModal")) {
-      document.getElementById("resumeModal").style.display = "none";
-    }
-  }
-  
+}
