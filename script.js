282a7b015b8915b47a1e82066a9b847d31aa641c
Kushal8616 <Kanjanap@flemingcollege.ca>
2025-04-05 18:16:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-// Resume Modal
-document.getElementById("resumeButton").onclick = function() {
-  document.getElementById("resumeModal").style.display = "flex";
-}
 
-document.getElementsByClassName("close")[0].onclick = function() {
-  document.getElementById("resumeModal").style.display = "none";
-}
-
-// Smooth Scroll for Navigation
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
-  });
-});
